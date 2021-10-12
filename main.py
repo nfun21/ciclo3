d295ed7a6ec514b9c7c20f0104901eba2cdee11b
AndresP <afp1996@hotmail.com>
2021-10-12 07:26:28 -0500

    Cambios 1

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
-def index():
-   return()
-    
+def paginaprincipal():
+   return render_template("pagina-principal.html")
+
+@app.route("/ingresar", methods = ["GET", "POST"])
+def ingresar():
+   return render_template("ingresar.html")
+
+@app.route("/registrarse", methods = ["GET", "POST"])
+def registrarse():
+   return render_template("registrarse.html")
+
+
+
