7377b3c2e0d4049ea78ca97991609bd8762ebe53
Nicolay F <nicolayf@uninorte.edu.co>
2021-10-13 16:33:22 -0500

    validaciones de crear/editar usuario + gestion vuelos/usuarios

--- main.py
+++ main.py
@@ -156,9 +156,14 @@ def recuperarcuenta():
 def superadmin():
    return render_template("superadmin.html")
 
+class FbuscarUsuario(FlaskForm):
+   nombreUsuario = StringField(label="nombreUsuario", validators=[DataRequired(message ='Es necesario digitar el nombre del usuario'), Length (min=3, max=120, message ='El nombre debe contener por lo menos %(min)d caracter/es y máximo %(max)d')])
+   botonEnviar = SubmitField(label='Consultar')
 @app.route("/gestion-usuarios", methods = ["GET", "POST"])
 def gestionusuarios():
-   return render_template("gestion-usuarios.html")
+   form = FbuscarUsuario()
+   form.validate_on_submit()
+   return render_template("gestion-usuarios.html", form=form)
 
 @app.route("/reviews", methods = ["GET", "POST"])
 def reviews():
@@ -166,15 +171,101 @@ def reviews():
 
 @app.route("/gestion-vuelos", methods = ["GET", "POST"])
 def gestionvuelos():
-   return render_template("gestion-vuelos.html")
+   form = cvueloform()
+   form.validate_on_submit()
+   return render_template("gestion-vuelos.html", form=form)
+
+class FcrearEditarUsuario(FlaskForm):
+   nombres=StringField(label='nombres',
+      validators=[
+         DataRequired(message='El campo de nombres no puede quedar vacío'),
+         Length(min=3, max=120, message='El nombre no puede tener menos de 3 caracteres y más de 120')
+      ]
+   )
+
+   apellidos=StringField(label='apellidos',
+      validators=[
+         DataRequired(message='El campo de apellidos no puede quedar vacío'),
+         Length(min=3, max=120, message='El apellido no puede tener menos de 3 caracteres y más de 120')
+      ]
+   )
+   cedula ='Cédula de ciudadania'
+   tarjetaId ='Tarjeta de identidad'
+   pasaporte = 'Pasaporte'
+   
+   tipoDocumento=SelectField(label='tipo doc',
+      choices=[
+         (cedula),
+         (tarjetaId),
+         (pasaporte)
+         ], 
+         validate_choice=True,
+         validators=[DataRequired(message='El campo de tipo de documento no puede quedar vacío')]
+         )
+         
+  
+   numDocumento = StringField(label='número doc',
+      validators=[
+         DataRequired(message='El campo de número de documento no puede quedar vacío'),
+         Length(min=8, max=70, message="La identificación introducida es muy larga o muy corta")
+      ]
+   )
+   
+   
+
+   genero= SelectField(label="género",
+      choices=[
+         ('Masculino'),
+         ('Femenino'),
+         ('Prefiero no decirlo')
+         ],
+      validate_choice=True,
+      validators=[
+         DataRequired(message='El campo de genero no puede quedar vacío')
+      ]
+   )
+   rol= SelectField(label="rol",
+      choices=[
+         ('Pasajero'),
+         ('Piloto'),
+         ('Superadministrador')
+         ],
+      validate_choice=True,
+      validators=[
+         DataRequired(message='El campo de rol no puede quedar vacío')
+      ]
+   )
+
+   fechaNacimiento = DateField(label="Fecha nacimiento",
+      
+      validators=[DataRequired(message='El campo de fecha de nacimiento no puede quedar vacío')]
+      )
+
+   telefono=StringField(label="telefono",
+      validators=[
+         DataRequired(message='El campo de teléfono no puede quedar vacío'),
+         Length(min=6,max=20, message='El número de teléfono es muy corto o muy largo')
+      ]
+   )
+   correo = StringField(label="correo",
+      validators=[DataRequired(message='El campo de correo no puede quedar vacío'),
+      Email(message='El correo no es válido'),
+      Length(min=8, max=120, message='El correo debe tener mínimo 8 caracteres y máximo 120')]
+      )
+   
+   botonEnviar = SubmitField(label="GUARDAR")
 
 @app.route("/crear-usuario", methods = ["GET", "POST"])
 def crearusuario():
-   return render_template("crear-usuario.html")
+   form = FcrearEditarUsuario()
+   form.validate_on_submit()
+   return render_template("crear-usuario.html", form=form)
 
 @app.route("/editar-usuario", methods = ["GET", "POST"])
 def editarusuario():
-   return render_template("editar-usuario.html")
+   form = FcrearEditarUsuario()
+   form.validate_on_submit()
+   return render_template("editar-usuario.html", form=form)
    
 @app.route("/eliminar-usuario", methods = ["GET", "POST"])
 def eliminarusuario():
